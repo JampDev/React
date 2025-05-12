d6a0f7ebf2eeed3c27891736259c44b2fefab726
unknown <jmorocho35@gmail.com>
2025-05-11 22:23:26 -0500

    JM: Primeros componentes y hocks

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//import {HelloWorldApp} from './HelloWorldApp'
+//import {FirstApp} from './FirstApp';
+import {CounterApp} from './CounterApp';
+
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>        
+        <CounterApp value = {45}/>
+    </React.StrictMode>
+);
